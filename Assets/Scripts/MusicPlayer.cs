a9b9a88522ec6a8d7128b6ae116f8556439b8196
Pete <pete@peter.com>
2018-02-20 14:56:40 +0100

    Fix sound, add ball bouncing on paddle

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -2,10 +2,19 @@
 using System.Collections;
 
 public class MusicPlayer : MonoBehaviour {
+	
+	static MusicPlayer instance = null;
 
-	// Use this for initialization
-	void Start () {
-		GameObject.DontDestroyOnLoad(gameObject);
+	void Awake () {
+		if (instance != null)
+		{
+			Destroy(gameObject);
+		}
+		else
+		{
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
 	}
 	
 	// Update is called once per frame
