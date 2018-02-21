bded5c8844871e7bada44d13e537d718ecd58ed7
Pete <pete@peter.com>
2018-02-21 14:30:14 +0100

    Add bricks, add Prefabs, add level2 - bricks not destroying

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	public Paddle paddle;
+	public bool hasStarted = false;
+	
+	public Vector3 paddleToBarVector;
+	// Use this for initialization
+	void Start () {
+		paddleToBarVector = this.transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(!hasStarted)
+		{
+			this.transform.position = paddleToBarVector + paddle.transform.position;
+
+			if(Input.GetMouseButtonDown(0))
+			{
+				this.rigidbody2D.velocity = new Vector2(2f, 10f);
+				hasStarted = true;
+			}
+		}
+	}
+}
